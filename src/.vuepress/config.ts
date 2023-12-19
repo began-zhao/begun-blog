72afbcc16d64b64786da31b837e4ff3bb3f8d4a8
z-begun <z-begun@outlook.com>
2023-12-19 14:42:28 +0800

    修改部分内容

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+//   设置正确的 base 选项。
+// 如果你准备发布到 https://<USERNAME>.github.io/ ，你可以省略这一步，因为 base 默认就是 "/" 。
+// 如果你准备发布到 https://<USERNAME>.github.io/<REPO>/ ，也就是说你的仓库地址是 https://github.com/<USERNAME>/<REPO> ，则将 base 设置为 "/<REPO>/"。
+  base: "/begun-blog/",
 
   locales: {
     "/": {
