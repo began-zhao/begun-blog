48b753cf047a7b56aec1e92a663e03023078f688
z-begun <z-begun@outlook.com>
2023-12-20 11:58:56 +0800

    评论内容增加

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -84,10 +84,10 @@ export default hopeTheme({
     // install @waline/client before enabling it
     // WARNING: This is a test server for demo only.
     // You should create and use your own comment service in production.
-    // comment: {
-    //   provider: "Waline",
-    //   serverURL: "https://waline-comment.vuejs.press",
-    // },
+    comment: {
+      provider: "Waline",
+      serverURL: "https://begun-blog-waline-i825eb77v-began-zhao.vercel.app/",
+    },
 
     // all features are enabled for demo, only preserve features you need here
     mdEnhance: {
