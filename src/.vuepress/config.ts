afb3eed88b1fcc81f63b723e45e6e835f1c8607c
z-begun <z-begun@outlook.com>
2023-12-22 15:28:50 +0800

    搜索参数更改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -20,7 +20,7 @@ export default defineUserConfig({
   plugins: [
     docsearchPlugin({
       appId: "JUMI1R306J",
-      apiKey: "74333c15f686403d2037832db7f836bd",
+      apiKey: "d3ecad32515453b2e7c430e080f1c0b7",
       indexName: "begun",
       locales: {
         "/": {
